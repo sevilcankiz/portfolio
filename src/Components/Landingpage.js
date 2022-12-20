352873ebf11d3bb5b6b87d88ddb39c9468c752d2
sevilcankiz <kumral-sevil21@hotmail.de>
2022-12-20 16:21:04 +0100

    update

--- src/Components/Landingpage.js
+++ src/Components/Landingpage.js
@@ -4,6 +4,7 @@ import Profilpicture from '../img/Sevil2.jpg';
 import Projekte from './Projekte';
 import GitHubIcon from '@mui/icons-material/GitHub';
 import LinkedInIcon from '@mui/icons-material/LinkedIn';
+import AlternateEmailIcon from '@mui/icons-material/AlternateEmail';
 
 
 export default function Landingpage() {
@@ -17,7 +18,7 @@ export default function Landingpage() {
         <section className='links'>
           <a href='https://github.com/sevilcankiz' target="_blank" rel="noreferrer" abbr title="GitHub"><GitHubIcon /></a>
           <a href='https://www.linkedin.com/in/sevil-sent%C3%BCrk-5b1b3b1b3/' target="_blank" rel="noreferrer" abbr title="LinkedIn"><LinkedInIcon /></a>
-          
+          <a href='mailto:sevilsentuerk@hotmail.com' target="_blank" rel="noreferrer" abbr title="E-Mail"><AlternateEmailIcon /></a>
         </section>
       </header>
       <Projekte />
--- src/Components/Projekte.js
+++ src/Components/Projekte.js
@@ -1,17 +1,26 @@
 import React from 'react'
 import '../Styles/projekte.css';
 import ToDo from '../img/To-Do.png';
+import Beispiel from '../img/b.jpg';
 
 export default function Projekte() {
   return (
     <main>
+        <h4>Projekte</h4>
         <section>
-            <h4>Projekte</h4>
-            <br/>
-            <a href="https://635563dfebafdd0c8e99bedc--clever-froyo-71659b.netlify.app/" target="_blank" rel="noreferrer">
-            <img src={ToDo} alt="projekt"/>
-            </a>
-            <p>To-Do list</p>
+          <div className="projects">
+            <div className='img-hover'>
+              <a href="https://635563dfebafdd0c8e99bedc--clever-froyo-71659b.netlify.app/" target="_blank" rel="noreferrer">
+              <img src={ToDo} alt="projekt"/></a>
+            </div>  
+              <p>To-Do list</p>
+          </div>
+          <div className="projects">
+            <div className='img-hover'>
+              <img src={Beispiel} alt="projekt"/>
+            </div>  
+              <p>To-Do list</p>
+          </div> 
         </section>
     </main>
   )
