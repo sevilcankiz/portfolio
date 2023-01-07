05443721eebf0e0fd312a108bfe86dab09434691
sevilcankiz <kumral-sevil21@hotmail.de>
2023-01-08 00:34:49 +0100

    projekt update

--- src/Components/Projekte.js
+++ src/Components/Projekte.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import '../Styles/projekte.css';
-import ToDo from '../img/To-Do.png';
-import Beispiel from '../img/b.jpg';
+import ToDo from '../img/To-Do.jpg';
+// import Beispiel from '../img/b.jpg';
 
 export default function Projekte() {
   return (
@@ -10,17 +10,17 @@ export default function Projekte() {
         <section>
           <div className="projects">
             <div className='img-hover'>
-              <a href="https://635563dfebafdd0c8e99bedc--clever-froyo-71659b.netlify.app/" target="_blank" rel="noreferrer">
+              <a href="https://clever-froyo-71659b.netlify.app/" target="_blank" rel="noreferrer">
               <img src={ToDo} alt="projekt"/></a>
             </div>  
               <p>To-Do list</p>
           </div>
-          <div className="projects">
+          {/* <div className="projects">
             <div className='img-hover'>
               <img src={Beispiel} alt="projekt"/>
             </div>  
               <p>To-Do list</p>
-          </div> 
+          </div>  */}
         </section>
     </main>
   )
